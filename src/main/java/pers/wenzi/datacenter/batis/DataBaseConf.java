516eed335921aafeccecefff8a640584ded13a3c
roger813 <roger813@163.com>
2016-03-15 23:04:50 +0800

    新建DataBaseConf.java

--- src/main/java/pers/wenzi/datacenter/batis/DataBaseConf.java
+++ src/main/java/pers/wenzi/datacenter/batis/DataBaseConf.java
@@ -5,31 +5,59 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 @Configuration
 @EnableTransactionManagement
-public class DataBaseConf {
+public class DataBaseConf implements TransactionManagementConfigurer{
   
-  private RelaxedPropertyResolver propertyResolver;
+  @Autowired
+  DataSource dataSource;
   
-  public void setEnvironment(Environment env) {
+  @Bean(name="sqlSessionFactory")
+  public SqlSessionFactory sqlSessionFactoryBean() {
+      
+    SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+    bean.setDataSource(dataSource);
+    bean.setTypeAliasesPackage("tk.mybatis.springboot.model");
     
-    this.propertyResolver = new RelaxedPropertyResolver(env, "jdbc.");
+    // 添加XML目录
+    ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+    try {
+      bean.setMapperLocations(resolver.getResources("classpath: mapper/*.xml"));
+      return bean.getObject();
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    } 
+      
+  }
+
+  @Bean
+  public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+    
+    return new SqlSessionTemplate(sqlSessionFactory);
     
   }
   
-  @Bean(name="readDataSources")
-  public List<DataSource> readDataSources() {
-    
-    List<DataSource> dataSources = new ArrayList<DataSource>();
-    
-    return dataSources;
-    
+  @Override
+  public PlatformTransactionManager annotationDrivenTransactionManager() {
+    // TODO Auto-generated method stub
+    return new DataSourceTransactionManager(dataSource);
   }
+  
 
 }
