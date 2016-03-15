d5dfcb0f85123970dfaa4a81399790925378171e
wenzi <roger813@163.com>
2016-03-15 18:15:17 +0800

    add application.yml&add DataBaseConf.java

--- src/main/java/pers/wenzi/datacenter/batis/DataBaseConf.java
+++ src/main/java/pers/wenzi/datacenter/batis/DataBaseConf.java
@@ -1,5 +1,35 @@
 package pers.wenzi.datacenter.batis;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
 public class DataBaseConf {
+  
+  private RelaxedPropertyResolver propertyResolver;
+  
+  public void setEnvironment(Environment env) {
+    
+    this.propertyResolver = new RelaxedPropertyResolver(env, "jdbc.");
+    
+  }
+  
+  @Bean(name="readDataSources")
+  public List<DataSource> readDataSources() {
+    
+    List<DataSource> dataSources = new ArrayList<DataSource>();
+    
+    return dataSources;
+    
+  }
 
 }
