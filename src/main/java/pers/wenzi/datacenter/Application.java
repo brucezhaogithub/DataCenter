6bca275ba950a50cb2c8710f887a254220d31180
wenzi <roger813@163.com>
2016-03-16 17:56:25 +0800

    添加信息：打印当前使用的属性文件

--- src/main/java/pers/wenzi/datacenter/Application.java
+++ src/main/java/pers/wenzi/datacenter/Application.java
@@ -2,6 +2,7 @@ package pers.wenzi.datacenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * itest自动化测试框架数据银行主入口
@@ -13,7 +14,11 @@ public class Application {
     
     public static void main(String[] args) {
       
-      SpringApplication.run(Application.class, args);
+      ApplicationContext ctx = SpringApplication.run(Application.class, args);
+//      String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+//      for (String profile : activeProfiles) {
+//        System.out.println("当前使用属性文件：" + profile);
+//      }
       
     }
     
