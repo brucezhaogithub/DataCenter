eb394dec15114df783b1998be01ee32535841d42
wenzi <roger813@163.com>
2016-03-16 19:57:23 +0800

    添加 EmbeddedServletContainerFactory Bean

--- src/main/java/pers/wenzi/datacenter/Application.java
+++ src/main/java/pers/wenzi/datacenter/Application.java
@@ -2,6 +2,9 @@ package pers.wenzi.datacenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * itest自动化测试框架数据银行主入口
@@ -14,6 +17,14 @@ public class Application {
     public static void main(String[] args) {
       
       SpringApplication.run(Application.class, args);
+      
+    }
+    
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+      
+      TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+      return factory;
       
     }
     
