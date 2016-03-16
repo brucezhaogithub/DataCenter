a06498beead6d65cf91a850fe3cc19740d67c629
wenzi <roger813@163.com>
2016-03-16 18:02:19 +0800

    就先这样吧

--- src/main/java/pers/wenzi/datacenter/Application.java
+++ src/main/java/pers/wenzi/datacenter/Application.java
@@ -2,7 +2,6 @@ package pers.wenzi.datacenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 /**
  * itest自动化测试框架数据银行主入口
