020bd448bf4987ca7a5529d47bfa40460920917d
wenzi <roger813@163.com>
2016-03-18 18:25:26 +0800

    算了，回家弄了

---
+++ src/main/java/pers/wenzi/datacenter/entity/PolicyEntity.java
@@ -0,0 +1,66 @@
+package pers.wenzi.datacenter.entity;
+
+public class PolicyEntity {
+
+  private String policyNo;
+  private String productId;
+  private String packageDefId;
+  private String campaignDefId;
+  
+  /*
+   * get
+   */
+  
+  public String getPolicyNo() {
+    
+    return policyNo;
+    
+  }
+  
+  public String getProductId() {
+    
+    return productId;
+    
+  }
+
+  public String getPackageDefId() {
+  
+    return packageDefId;
+  
+  }
+
+  public String getCampaignDefId() {
+  
+    return campaignDefId;
+  
+  }
+  
+  /*
+   * set
+   */
+  
+public void setPolicyNo(String policyNo) {
+    
+    this.policyNo = policyNo;
+    
+  }
+  
+  public void setProductId(String productId) {
+    
+    this.productId = productId;
+    
+  }
+
+  public void setPackageDefId(String packageDefId) {
+  
+    this.packageDefId = packageDefId;
+  
+  }
+
+  public void setCampaignDefId(String campaignDefId) {
+  
+    this.campaignDefId = campaignDefId;
+  
+  }
+  
+}
