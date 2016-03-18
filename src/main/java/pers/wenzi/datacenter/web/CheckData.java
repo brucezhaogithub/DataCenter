ea21b16d6b425e862f885fb3a0b9d03e99d89753
wenzi <roger813@163.com>
2016-03-18 17:18:47 +0800

    添加数据库查询

--- src/main/java/pers/wenzi/datacenter/web/CheckData.java
+++ src/main/java/pers/wenzi/datacenter/web/CheckData.java
@@ -1,7 +1,13 @@
 package pers.wenzi.datacenter.web;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +20,23 @@ public class CheckData {
   // 官网-女性疾病-数据校验
   @RequestMapping(value="/Online-NXJB/{policyNo}", method=RequestMethod.GET)
   public String submitData(Map<String, String> model, 
-      @PathVariable("policyNo") String policyNo) {
+      @PathVariable("policyNo") String policyNo) throws IOException {
     
-//    String resource = "pers/wenzi/datacenter/batis/BatisConf.xml";
-//    System.out.println("resource path is:" + this.getClass().getClassLoader().getResourceAsStream(resource));
-//    InputStream is = this.getClass().getClassLoader().getResourceAsStream(resource);
-//    SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
-//    SqlSession session = sessionFactory.openSession();
-//    String statement = "pers.wenzi.datacenter.mapper.PolicyMapper.getPolicy";
-//    PolicyEntity policyEntity = session.selectOne(statement, 1);
+    String                    resource  = "/batisconf.xml";
+    System.out.println("resource path is:" + CheckData.class.getResourceAsStream(resource));
+    InputStream               is        = Resources.getResourceAsStream(resource);
+    SqlSessionFactoryBuilder  builder   = new SqlSessionFactoryBuilder();
+    SqlSessionFactory         factory   = builder.build(is);
+    SqlSession                session   = factory.openSession();
+    String statement = "pers.wenzi.datacenter.mapper.PolicyMapper.getPolicy";
+    String result = session.selectOne(statement);
+  //    PolicyEntity policyEntity = session.selectOne(statement, 1);
 //    model.put("policyId", policyEntity.getPolicyId());
 //    model.put("policyNo", policyEntity.getPolicyNo());
 //    model.put("productId", policyEntity.getProductId());
 //    model.put("packageDefId", policyEntity.getPackageDefId());
 //    model.put("campaignDefId", policyEntity.getCampaignDefId());
+    model.put("policyId", result);
     model.put("policyNo", policyNo);
     return "checkdata";
     
