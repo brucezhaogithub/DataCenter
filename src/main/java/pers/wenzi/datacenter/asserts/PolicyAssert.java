342b49345e38cad887ae8274f3e24a0013dff075
wenzi <roger813@163.com>
2016-03-21 09:50:28 +0800

    调整 Assert 输出格式

--- src/main/java/pers/wenzi/datacenter/asserts/PolicyAssert.java
+++ src/main/java/pers/wenzi/datacenter/asserts/PolicyAssert.java
@@ -8,43 +8,22 @@ import pers.wenzi.datacenter.entity.PolicyEntity;
 
 public class PolicyAssert {
   
-  private String expProductId;
-  private String expPackageDefId;
-  private String expCampaignDefId;
-  private String actPolicyId;
-  private String actPolicyNo;
   private String actProductId;
   private String actPackageDefId;
   private String actCampaignDefId;
+  private String expProductId;
+  private String expPackageDefId;
+  private String expCampaignDefId;
   
-  public PolicyAssert(String checkName, String policyNo) {
+  public PolicyAssert() {
     
-    this.actPolicyNo = policyNo;
-    if ("OnlineNXJB".equals(checkName)) { setValueForOnlineNXJB(); }
     
-  }
-  
-  private void setValueForOnlineNXJB() {
-    
-    Entity entity         = new PolicyEntity();
-    String stmt           = "pers.wenzi.datacenter.mapper.PolicyMapper.selectPolicy";
-    entity                = SessionFactory.getSessionEntity(entity, stmt, this.actPolicyNo);
-    this.actPolicyId      = entity.getPolicyId();
-    this.actProductId     = entity.getProductId();
-    this.actPackageDefId  = entity.getPackageDefId();
-    this.actCampaignDefId = entity.getCampaignDefId();
-    this.expProductId     = "";
-    this.expPackageDefId  = "";
-    this.expCampaignDefId = "";
     
   }
   
-  
-  
-  public String AssertToString() {
+  public String AssertToString(String checkObj, String actValue, String expValue, String result) {
     
-    String msg = null;
-    return msg;
+    return new String(checkObj + "实际结果:" + actValue + "; 预期结果：" + expValue + "; " + result);
     
   }
   
