9d67f80438ea9941f0d06e410aff7a02c3d8b57b
wenzi <roger813@163.com>
2016-03-31 20:42:51 +0800

    添加投被保人信息实体类

---
+++ src/main/java/pers/wenzi/datacenter/model/InsuredContext.java
@@ -0,0 +1,118 @@
+package pers.wenzi.datacenter.model;
+
+public class InsuredContext {
+  
+  private String tbrName;
+  private String tbrCerti;
+  private String tbrPhone;
+  private String tbrEmail;
+  private String bbrName;
+  private String bbrCerti;
+  private String bbrPhone;
+  private String bbrEmail;
+  
+  /*
+   * getter method
+   */
+  
+  public String getTbrName() {
+    
+    return tbrName;
+    
+  }
+  
+  public String getTbrCerti() {
+    
+    return tbrCerti;
+    
+  }
+  
+  public String getTbrPhone() {
+    
+    return tbrPhone;
+    
+  }
+  
+  public String getTbrEmail() {
+    
+    return tbrEmail;
+    
+  }
+  
+  public String getBbrName() {
+    
+    return bbrName;
+    
+  }
+  
+  public String getBbrCerti() {
+    
+    return bbrCerti;
+    
+  }
+  
+  public String getBbrPhone() {
+    
+    return bbrPhone;
+    
+  }
+  
+  public String getBbrEmail() {
+    
+    return bbrEmail;
+    
+  }
+  
+  /*
+   * setter method
+   */
+  
+  public void setTbrName(String tbrName) {
+    
+    this.tbrName = tbrName;
+    
+  }
+  
+  public void setTbrCerti(String tbrCerti) {
+    
+    this.tbrCerti = tbrCerti;
+    
+  }
+  
+  public void setTbrPhone(String tbrPhone) {
+    
+    this.tbrPhone = tbrPhone;
+    
+  }
+  
+  public void setTbrEmail(String tbrEmail) {
+    
+    this.tbrEmail = tbrEmail;
+    
+  }
+  
+  public void setBbrName(String bbrName) {
+    
+    this.bbrName = bbrName;
+    
+  }
+  
+  public void setBbrCerti(String bbrCerti) {
+    
+    this.bbrCerti = bbrCerti;
+    
+  }
+  
+  public void setBbrPhone(String bbrPhone) {
+    
+    this.bbrPhone = bbrPhone;
+    
+  }
+  
+  public void setBbrEmail(String bbrEmail) {
+    
+    this.bbrEmail = bbrEmail;
+    
+  }
+  
+}
