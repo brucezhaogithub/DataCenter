866fe95739e041a047af6c57741298d1f5cc893e
wenzi <roger813@163.com>
2016-04-05 17:51:42 +0800

    修改日志等级

--- src/main/java/pers/wenzi/datacenter/web/GetData.java
+++ src/main/java/pers/wenzi/datacenter/web/GetData.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import pers.wenzi.datacenter.util.CertiUtil;
