ce59567f6660dc5450f33b125ff4edf90ada8453
wenzi <roger813@163.com>
2016-04-05 10:20:12 +0800

    适应环境

--- src/main/java/pers/wenzi/datacenter/util/InsuredUtil.java
+++ src/main/java/pers/wenzi/datacenter/util/InsuredUtil.java
@@ -1,10 +1,7 @@
 package pers.wenzi.datacenter.util;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
