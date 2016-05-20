d0731ba24910cdc579a82d1f32f6ac9d78025d4f
wenzi <roger813@163.com>
2016-05-20 16:47:19 +0800

    新增手网银行卡盗刷险校验方法

--- src/main/java/pers/wenzi/datacenter/web/CheckData.java
+++ src/main/java/pers/wenzi/datacenter/web/CheckData.java
@@ -20,6 +20,19 @@ public class CheckData {
   @Autowired
   private PolicyService policyService;
   
+  @RequestMapping(value="/mobile/yhds/plana/{policyNo}", method=RequestMethod.GET)
+  public String checkYhdsPlana(Map<String, String> model,
+      @PathVariable("policyNo") String policyNo) {
+    
+    policy = policyService.selectPolicy(policyNo);
+    model.put("policyNo",       policyNo);
+    model.put("productId",      policy.getProductId());
+    model.put("packageDefId",   policy.getPackageDefId());
+    model.put("campaignDefId",  policy.getCampaignDefId());
+    return "checkdata";
+    
+  }
+  
   // 官网-女性疾病-数据校验
   @RequestMapping(value="/online/nxjb/{policyNo}", method=RequestMethod.GET)
   public String checkNXJB(Map<String, String> map, 
@@ -31,6 +44,14 @@ public class CheckData {
     map.put("packageDefId",   policy.getPackageDefId());
     map.put("campaignDefId",  policy.getCampaignDefId());
     return "checkdata";
+    
+  }
+  
+  @RequestMapping(value="/checkpolicy", method=RequestMethod.POST)
+  public String checkPolicy(Map<String, String> map) {
+    
+    System.out.println("submit this");
+    return "checkpolicy";
     
   }
 
